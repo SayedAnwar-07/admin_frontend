8498745257b421ee37aa78db547dbd34b5964df3
SayedAnwarOfficial <sayedanwarofficial@gmail.com>
2026-08-17 14:22:44 +0600

    port change to 500

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,15 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+
+  server: {
+    port: 5000,
+    open: true,
   },
 });
